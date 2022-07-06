ecbe96a04a8e289a042fe5d8ee36494678c04a4a
unknown <annaglima@outlook.com>
2022-07-06 23:12:21 +0200

    Added files 34,36,38 to homework5

---
+++ exercise34/Program.cs
@@ -0,0 +1,36 @@
+﻿/* task 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая 
+покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+//Массив
+int[]array= new int[20];
+int evenNumbers = 0;
+
+
+for (int i=0; i<array.Length; i++)
+{   
+    array[i]=new Random().Next(100,1000);
+}
+PrintArray(array);
+   
+void PrintArray(int[]array)
+{
+    for(int i=0; i<array.Length;i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+}
+
+//Четные числа в массиве
+for (int i=0; i<array.Length; i++)
+{
+     if (array[i] %2 == 0)
+     {
+        Console.Write(array[i] + " ");
+     evenNumbers ++;
+     }
+}
+
+Console.WriteLine("There are "+ evenNumbers + " even numbers");
---
+++ exercise36/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+//Массив
+int[]array= new int[5];
+int sumNumbers = 0;
+
+
+for (int i=0; i<array.Length; i++)
+{   
+    array[i]=new Random().Next(int.MinValue,int.MaxValue);
+}
+PrintArray(array);
+   
+void PrintArray(int[]array)
+{
+    for(int i=0; i<array.Length;i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+}
+
+//Сумма чисел на нечетных позициях в массиве
+for (int i = 0; i<array.Length; i++)
+{
+    if (i % 2 != 0)
+      {
+        sumNumbers = sumNumbers + array[i];
+      }
+}
+
+Console.WriteLine("Answer is: " + sumNumbers);
+
+
